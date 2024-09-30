e0dc831bc63fb584ad9d6786f04f0070283bfbf4
prajjwaldevkota <68115199+prajjwaldevkota@users.noreply.github.com>
2024-09-30 17:07:39 -0400

    added schema for user login

--- PCPartPicker_Backend/schema.js
+++ PCPartPicker_Backend/schema.js
@@ -1,4 +1,24 @@
 export const schema = `
-type 
+type User {
+    id: ID
+    firstname: String
+    lastname: String
+    email: String
+    age:String
+    gender: String
+    membership: String
+    number:String
+    password: String
+}, 
+type AuthPayload {
+    token: String
+    user: User
+    admin: Admin
+    errorMessage: String 
+},
+type Mutation {
+    signup(firstname: String, lastname: String, email: String, age:String, gender:String, membership:String, number: String,  password: String): AuthPayload
+    login(email: String, password: String): AuthPayload
+}
 
-`
+`;

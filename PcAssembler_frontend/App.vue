47c5f71b211d4f4514e2303f067335996252f986
Orochimaru67 <138917370+Orochimaru67@users.noreply.github.com>
2024-10-01 01:45:35 -0400

    Add files via upload

---
+++ PcAssembler_frontend/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  background-color: #000; /* Black background */
+  color: #fff; /* White text */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
